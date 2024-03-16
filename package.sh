b4f1a58aafe2b1be67d5879faeb1453bfc3e6796
super5xy <super5xy@163.com>
2024-03-16 12:24:14 +0800

    fix compile

--- package.sh
+++ package.sh
@@ -2,3 +2,12 @@
 git clone  https://github.com/bigbugcc/OpenwrtApp package/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone  https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
